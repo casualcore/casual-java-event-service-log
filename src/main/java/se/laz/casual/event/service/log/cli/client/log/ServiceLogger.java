b1e2ec568017dcb07194ab01fcdce6e3527b8f63
cklellie <christopherjkelly@gmail.com>
2024-05-15 14:09:43 +0200

    Add flush to the log writing.

--- src/main/java/se/laz/casual/event/service/log/cli/client/log/ServiceLogger.java
+++ src/main/java/se/laz/casual/event/service/log/cli/client/log/ServiceLogger.java
@@ -96,6 +96,7 @@ public class ServiceLogger
         synchronized( fileWriterLock )
         {
             fileWriter.println( ServiceCallEventFormatter.format( event, delimiter ) );
+            this.fileWriter.flush();
         }
     }
 
